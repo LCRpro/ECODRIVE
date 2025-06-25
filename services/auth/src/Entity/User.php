71e71fbe2c53accbf5c1aa1b427291fcdfb5804a
LCR <cariou.liam@orange.fr>
2025-06-25 22:56:08 +0200

    création de l'entité user

--- services/auth/src/Entity/User.php
+++ services/auth/src/Entity/User.php
@@ -4,19 +4,20 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $googleId = null;
 
     #[ORM\Column(length: 255, nullable: true)]
@@ -38,7 +39,6 @@ class User
     public function setEmail(string $email): static
     {
         $this->email = $email;
-
         return $this;
     }
 
@@ -50,7 +50,6 @@ class User
     public function setGoogleId(string $googleId): static
     {
         $this->googleId = $googleId;
-
         return $this;
     }
 
@@ -62,19 +61,49 @@ class User
     public function setName(?string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+
+        if (!in_array('ROLE_USER', $roles, true)) {
+            $roles[] = 'ROLE_USER';
+        }
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
-
         return $this;
+    }
+
+ 
+    public function getUserIdentifier(): string
+    {
+        return $this->email ?? '';
+    }
+
+ 
+    public function getUsername(): string
+    {
+        return $this->email ?? '';
+    }
+
+    public function getPassword(): ?string
+    {
+        return null; 
+    }
+
+    public function getSalt(): ?string
+    {
+        return null; 
+    }
+
+    public function eraseCredentials(): void
+    {
     }
 }
