a93790601bb72989bb2e88c83445dba423ab6458
LCR <cariou.liam@orange.fr>
2025-06-25 23:28:51 +0200

    ajoute de la dep google api + correction du bug du au manque de la dep

--- services/auth/src/Service/GoogleOAuthService.php
+++ services/auth/src/Service/GoogleOAuthService.php
@@ -1,13 +1,14 @@
 <?php
 
-
 namespace App\Service;
+
+use Google_Client;
 
 class GoogleOAuthService
 {
     public function verify(string $idToken): array
     {
-        $client = new \Google_Client(['client_id' => $_ENV['GOOGLE_CLIENT_ID']]);
+        $client = new Google_Client(['client_id' => $_ENV['GOOGLE_CLIENT_ID']]);
         $payload = $client->verifyIdToken($idToken);
         if (!$payload) {
             throw new \Exception('Invalid Google ID token.');
