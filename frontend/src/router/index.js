6da4a5e13463932d9cdb787cd8973e313a121498
LCR <cariou.liam@orange.fr>
2025-06-26 00:36:01 +0200

    mise a jour du router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,13 +2,38 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
 import Profile from '../views/Profile.vue'
+import ProfileComplete from '../views/ProfileComplete.vue'
 
 const isAuthenticated = () => !!localStorage.getItem('token')
 
+const requireAuth = (to, from, next) => {
+  isAuthenticated() ? next() : next('/login')
+}
+
+const guardAuth = async (to, from, next) => {
+  if (!isAuthenticated()) return next('/login')
+
+  try {
+    const res = await fetch('http://localhost:8000/me', {
+      headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
+    })
+    const user = await res.json()
+
+    if (!user.name || !user.birthday || !user.gender || !user.address) {
+      return next('/profile/complete')
+    } else {
+      return next()
+    }
+  } catch {
+    return next('/login')
+  }
+}
+
 const routes = [
-  { path: '/', component: Home, beforeEnter: (to, from, next) => isAuthenticated() ? next() : next('/login') },
+  { path: '/', component: Home, beforeEnter: guardAuth },
   { path: '/login', component: Login },
-  { path: '/me', component: Profile, beforeEnter: (to, from, next) => isAuthenticated() ? next() : next('/login') },
+  { path: '/me', component: Profile, beforeEnter: requireAuth },
+  { path: '/profile/complete', component: ProfileComplete, beforeEnter: requireAuth }
 ]
 
 export default createRouter({
