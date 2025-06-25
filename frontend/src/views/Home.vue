66dad641af6768b923dda58a7235bdfbd5b195f0
LCR <cariou.liam@orange.fr>
2025-06-25 23:59:19 +0200

    home vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-semibold mb-4">Bienvenue sur Ecodrive</h1>
+    <router-link to="/me" class="underline text-blue-500">Voir mon profil</router-link>
+  </div>
+</template>
