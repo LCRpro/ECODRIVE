1016eb75b32a5670ea37483236befe35b91d7727
LCR <cariou.liam@orange.fr>
2025-06-25 23:28:30 +0200

    frontend call pour se connecter avec google

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import LoginView from './views/LoginView.vue'
+
 </script>
 
 <template>
@@ -13,7 +15,8 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+     <LoginView />
+
   </main>
 </template>
 
